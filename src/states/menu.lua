a59a4a5574ee15a63e0f4762f9ce3747107b87ea
Lauren H <158091371+Lh4355@users.noreply.github.com>
2025-12-05 21:06:58 -0500

    Added image to main menu

--- src/states/menu.lua
+++ src/states/menu.lua
@@ -1,6 +1,12 @@
 -- src/menu.lua
 local Menu = {}
 
+local background_image
+
+function Menu.enter()
+	background_image = love.graphics.newImage("src/data/images/image1.png")
+end
+
 function Menu.update(dt)
 	-- Add animation logic here later (e.g., bouncing text)
 end
@@ -11,11 +17,26 @@ function Menu.draw()
 
 	love.graphics.setColor(1, 1, 1) -- White
 
+	-- Draw the background image FIRST (so it sits behind the text)
+    if background_image then
+        -- Calculate scale factor to make image fit the screen
+        local sx = windowWidth / background_image:getWidth()
+        local sy = windowHeight / background_image:getHeight()
+        
+        -- Draw image at 0,0 with scale factors sx and sy
+        love.graphics.draw(background_image, 0, 0, 0, sx, sy)
+    end
+
 	-- Draw Title (Centered)
 	local title = "The Rolling Can"
 	local font = love.graphics.getFont()
 	local titleWidth = font:getWidth(title)
-	love.graphics.print(title, (windowWidth - titleWidth) / 2, 200)
+
+	love.graphics.setColor(0, 0, 0) -- Black shadow?
+    love.graphics.print(title, (windowWidth - titleWidth) / 2 + 2, 202)
+    
+    love.graphics.setColor(1, 1, 1) -- White text
+    love.graphics.print(title, (windowWidth - titleWidth) / 2, 200)
 
 	-- Draw Instruction
 	local text = "Press ENTER or CLICK to Start"
